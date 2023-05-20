fd2b89e97f00980db9796fbde965083055d964cb
MagnificentCreature <E0960127@u.nus.edu>
2023-05-20 19:23:46 +0800

    Little things, starting the room skeleton

--- Controller/BotCommands.py
+++ Controller/BotCommands.py
@@ -9,7 +9,12 @@ import sys
 from pathlib import Path
 sys.path.insert(1, str(Path(__file__).parent.parent.absolute()))
 from Chat.DialogueReader import DialogueReader
+from Room.RoomHandler import RoomHandler
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await DialogueReader.sendMessage(context.bot, update, "Welcome1")
     await DialogueReader.sendMessage(context.bot, update, "Welcome2")
+
+async def create_room(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await DialogueReader.sendMessage(context.bot, update, "Creating Room")
+    room = RoomHandler().generateRoom(update.effective_user)
