01313bda6697f5ca459fed63820c3a2c8a1bb356
xu-wenho <ho.xu.wen@gmail.com>
2023-06-21 15:18:05 +0800

    added methods for Image class

--- BotController/BotCommands.py
+++ BotController/BotCommands.py
@@ -110,6 +110,8 @@ async def take_prompt(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # context.user_data['imageURL'] = imageURL
     # await DialogueReader.sendImageURLByID(context.bot, update.message.from_user.id, imageURL)
 
+    
+
     waitingID = await DialogueReader.sendMessageByID(context.bot, update.message.from_user.id, "WaitingForItems", **{'item': "prompt"})     #TODO find a way to delete this message when the next phase starts
     await context.bot.send_message(chat_id=update.effective_chat.id, text='The image you generated: ' + context.user_data['prompt'])
     await RoomHandler.checkItems(context.user_data['roomCode'], Player.PlayerConstants.PROMPT, context.bot)
--- GameController/Image.py
+++ GameController/Image.py
@@ -6,3 +6,16 @@ class Image:
     prompt = "a"
     imageURL = "a" #store imageURL
     imageLies = [] #store imageLies as a tuple of (lie, player)
+
+    async def __init__(self, author, prompt, imageURL):
+        self.author = author
+        self.prompt = prompt
+        self.imageURL = imageURL
+        self.imageLies = []
+
+    # for each image, add a tuple for lies given by other players
+    async def insertLie(self, lie, username):
+        self.imageLies.append((lie, username))
+
+
+    
