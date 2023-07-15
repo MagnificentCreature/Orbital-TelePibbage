6216a1d8459fc9d07b2968627474defccfd4aa01
MagnificentCreature <E0960127@u.nus.edu>
2023-07-15 23:11:48 +0800

    added prepositions

--- GameController/ArcadePrompt.py
+++ GameController/ArcadePrompt.py
@@ -3,11 +3,12 @@ import random
 from random_word import Wordnik
 
 PROMPT_POOL = 'Assets\PromptsGenerationPool.csv'
-PREPOSITIONS = 'Assets\Prepositions.csv'
+PREPOSITIONS = 'Assets\Preposition.csv'
 MIN_CORPUS_COUNT = 10000
 WORDNIK_API_KEY = "zqai8u9e5pi2p1ia43ee0359w7loishvhyoll5k06hk5j3ges"
 
-def process_dataframe(df):
+@staticmethod
+def process_generation_poll(df):
     data = {}
     # Initialize current subgroup for each column
     current_subgroup = {column: None for column in df.columns}
@@ -37,6 +38,15 @@ def process_dataframe(df):
                 subgroup_score = int(subgroup_score.strip("()"))
                 data[(header, header_score)][(subgroup, subgroup_score)].append(row[column])
     return data
+
+@staticmethod
+def process_prepositions():
+    data = []
+    with open(PREPOSITIONS, 'r') as f:
+        prepositions = f.readlines()
+        data = prepositions[0].split(',')
+    return data
+
 
 def get_random_dict(data, banned=None):
     total_score = sum(score for (header, score) in data.keys() if header != banned)
@@ -94,9 +104,10 @@ wordnik_service = Wordnik(api_key=WORDNIK_API_KEY)
 def get_random_word():
     return wordnik_service.get_random_word(hasDictionaryDef="true", includePartOfSpeech="noun,verb", minCorpusCount=MIN_CORPUS_COUNT, minDictionaryCount=20)
 
-def get_random_word_list():
-    word_list = wordnik_service.get_random_words(hasDictionaryDef="true", includePartOfSpeech="noun,verb", minCorpusCount=MIN_CORPUS_COUNT, minDictionaryCount=20, limit=6)
-    while len(word_list) < 6:
+#Should I make it more common for nouns to show up (ie change ratio of nouns to verbs?)
+def get_random_word_list(length=6):
+    word_list = wordnik_service.get_random_words(hasDictionaryDef="true", includePartOfSpeech="noun,verb", minCorpusCount=MIN_CORPUS_COUNT, minDictionaryCount=20, limit=length)
+    while len(word_list) < length:
         word_list.append(get_random_word())
     return word_list
 
@@ -116,21 +127,37 @@ def prompt_user(word_list):
     print("You chose:", chosen_word)
     return chosen_word
 
+def comma_seperated_output(str1, str2, str3):
+    return f"{str1}, {str2}, {str3}"
+
+def randomize_strings(str1, str2, str3):
+    prepositions = process_prepositions()
+    strings = [str1, str2, str3]
+    random.shuffle(strings)
+    result = ""
+    for i in range(len(strings) - 1):
+        result += strings[i] + " " +  random.choice(prepositions) + " "
+    result += strings[-1]
+    return result
+    # return f"{strings[0]} {random.choice(prepositions)} {strings[1]} + {random.choice(prepositions)} + {strings[2]}"
+
 if __name__ == "__main__":
     df = pd.read_csv(PROMPT_POOL)
-    data = process_dataframe(df)
-
-    word_list = get_random_word_list()
-    choosen_word1 = prompt_user(word_list)
+    data = process_generation_poll(df)
 
     curated_word_dict = get_random_elements_dict(data)
-    choosen_word2 = prompt_user(list(curated_word_dict.keys()))
-    print(curated_word_dict[choosen_word2])
+    choosen_word1 = prompt_user(list(curated_word_dict.keys()))
 
-    curated_word_list2 = get_random_elements(data, num_elements=5, banned=curated_word_dict[choosen_word2])
-    curated_word_list2.append(get_random_word())
+    word_list = get_random_word_list()
+    choosen_word2 = prompt_user(word_list)
+
+    # curated_word_list2 = get_random_elements(data, num_elements=5, banned=curated_word_dict[choosen_word1])
+    curated_word_list2 = get_random_elements(data, num_elements=3, banned=curated_word_dict[choosen_word1])
+    curated_word_list2.extend(get_random_word_list(length=3))
     choosen_word3 = prompt_user(curated_word_list2)
 
-    print("Final prompt is:" + choosen_word1 + ", " + choosen_word2 + ", " + choosen_word3)
-    
+    print("Comma seperated prompt is: " + choosen_word1 + ", " + choosen_word2 + ", " + choosen_word3)
+    print("Random preposition prompt is: " + randomize_strings(choosen_word1, choosen_word2, choosen_word3))
+    print("Random preposition prompt is: " + randomize_strings(choosen_word1, choosen_word2, choosen_word3))
+
 
